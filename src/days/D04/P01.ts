26e2884c05e23631142b9be790bfc842161d4ca0
Christoffer Lund <chrislund90@gmail.com>
2023-12-04 06:17:29 +0100

    Day 4 Part 1 solved

--- src/days/D04/P01.ts
+++ src/days/D04/P01.ts
@@ -1,5 +1,31 @@
+import { log } from 'console';
 import { Utils } from '../../utils';
 
 export const solve = (input: string): string => {
-    return 'Not implemented';
+    const lines = input.split('\n');
+
+    let points = 0;
+    lines.forEach((line, i) => {
+        const winningNumbers = line
+            .split('|')[0]
+            .split(':')[1]
+            .split(' ')
+            .filter((e) => e != '');
+
+        const myNumbers = line
+            .split('|')[1]
+            .split(' ')
+            .filter((e) => e != '');
+
+        let numberOfMatches = 0;
+        myNumbers.forEach((number) => {
+            if (winningNumbers.some((wn) => wn == number)) {
+                numberOfMatches++;
+            }
+        });
+
+        points += Math.floor(Math.pow(2, numberOfMatches - 1));
+    });
+
+    return '' + points;
 };
