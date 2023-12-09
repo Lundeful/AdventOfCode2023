1e190ccbfb2a9fdd5b063b2b7b1e26abb53763a0
Christoffer Lund <chrislund90@gmail.com>
2023-12-09 06:39:25 +0100

    Solved Day 9 Part 1

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { runSolver } from './src/runSolver';
 import { VerifySetup } from './src/verifySetup';
 
 const main = async () => {
-    const day = 8;
+    const day = 9;
     const part = 1;
 
     console.log(`Running${colors.yellow} Day ${day} Part ${part}${colors.default}`);
--- src/days/D09/P01.ts
+++ src/days/D09/P01.ts
@@ -1,5 +1,53 @@
+import { log } from 'console';
 import { Utils } from '../../utils';
 
 export const solve = (input: string): string => {
-    return 'Not implemented';
+    const lines = input.split('\n');
+    const sum = lines
+        .map((line) => line.split(' ').map((n) => parseInt(n)))
+        .map((history) => getNextValueForHistory(history))
+        .reduce(Utils.Reducers.add);
+
+    return '' + sum;
+};
+
+const getNextValueForHistory = (numbers: number[]): number => {
+    const grid = fillGrid(numbers);
+
+    grid.forEach((line) => {
+        line.push(0);
+    });
+
+    for (let i = grid.length - 1; i > 0; i--) {
+        const currentRow = grid[i];
+        const rowAbove = grid[i - 1];
+        const currrentRowValue = currentRow[currentRow.length - 1];
+        const topLeftValue = rowAbove[rowAbove.length - 2];
+        rowAbove[rowAbove.length - 1] = topLeftValue + currrentRowValue;
+    }
+
+    const nextValue = grid[0][grid[0].length - 1];
+    return nextValue;
+};
+
+const fillGrid = (numbers: number[]): number[][] => {
+    const grid: number[][] = [numbers];
+
+    let index = 0;
+    while (grid[index].some((n) => n != 0)) {
+        const previousArr = grid[index];
+        const newArr = Array(grid[index].length - 1).fill(0);
+
+        for (let j = 0; j < newArr.length; j++) {
+            const element = newArr[j];
+            const nextValue = previousArr[j + 1] - previousArr[j];
+            newArr[j] = nextValue;
+        }
+
+        grid.push(newArr);
+
+        index++;
+    }
+
+    return grid;
 };
